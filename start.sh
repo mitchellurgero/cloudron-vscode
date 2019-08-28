048dda3033679c6162c096ed46a165cb56247f66
Mitchell Urgero <info@urgero.org>
2019-08-28 11:53:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ chown -R cloudron:cloudron /app/data
 cd /app/data
 
 ## Launch VSCode, set port, and password for authentication.
-APACHE_CONFDIR="" source /etc/apache2/envvars
-rm -f "${APACHE_PID_FILE}"
-exec /usr/sbin/apache2 -DFOREGROUND &
+#APACHE_CONFDIR="" source /etc/apache2/envvars
+#rm -f "${APACHE_PID_FILE}"
+#exec /usr/sbin/apache2 -DFOREGROUND &
 
-exec /usr/local/bin/gosu cloudron:cloudron /app/code/code-server /app/data/workdir --port=8000 --allow-http --data-dir=/app/data --trust-proxy --no-auth
+exec /usr/local/bin/gosu cloudron:cloudron /app/code/code-server /app/data/workdir --port=8000 --allow-http --data-dir=/app/data --trust-proxy
